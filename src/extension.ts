728ca24f28ed952cfb7a430bd03505d1c13f260b
yutotnh <57719497+yutotnh@users.noreply.github.com>
2024-01-12 01:00:28 +0900

    feat: 文字列を区切る関連の関数を追加

--- src/extension.ts
+++ src/extension.ts
@@ -24,5 +24,62 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
+/**
+ * 正規表現用にエスケープする
+ * @param string エスケープする文字列
+ */
+export function escapeRegExp(string: string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
+/**
+ * 単語の区切り文字の正規表現を取得する
+ * @param wordSeparetors 単語の区切り文字
+ */
+export function getWordSeparatorsRegExp(wordSeparetors: string) {
+  const wordSeparatorsArray = wordSeparetors.split("");
+  let wordSeparatorsRegExp = "([";
+
+  for (const wordSeparator of wordSeparatorsArray) {
+    wordSeparatorsRegExp += escapeRegExp(wordSeparator);
+  }
+
+  wordSeparatorsRegExp += "]+)";
+
+  return new RegExp(wordSeparatorsRegExp, "g");
+}
+
+/**
+ * スペースで分割する
+ * - スペースは残る
+ * - スペースのみの要素が連続している場合に、連続したスペースの要素を1つにまとめる
+ * @param stringArray スペースで分割する文字列の配列
+ *
+ * @todo スペースのみの要素が連続している場合に、連続したスペースの要素を1つにまとめる
+ */
+export function splitBySpace(stringArray: string[]) {
+  let result: string[] = [];
+
+  for (const string of stringArray) {
+    result = result.concat(string.split(/([\s]+)/g).filter((word) => word));
+  }
+
+  // スペースのみの要素が連続している場合に、連続したスペースの要素を1つにまとめる
+  // 入力: [" ", " ", "a", " ", " ", "b", " ", " ", "c", " "]
+  // 期待値: ["  ", "a", "  ", "b", "  ", "c", " "]
+
+  const pattern = /^\s+$/g;
+  for (let i = 0; i < result.length - 1; i++) {
+    if (result[i].match(pattern)) {
+      if (result[i + 1].match(pattern)) {
+        result[i] = result[i] + result[i + 1];
+        result.splice(i + 1, 1);
+      }
+    }
+  }
+
+  return result;
+}
+
 // This method is called when your extension is deactivated
 export function deactivate() {}
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -3,6 +3,7 @@ import * as assert from "assert";
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from "vscode";
+import * as extension from "../extension";
 // import * as myExtension from '../../extension';
 
 suite("Extension Test Suite", () => {
@@ -11,5 +12,114 @@ suite("Extension Test Suite", () => {
   test("Sample test", () => {
     assert.strictEqual(-1, [1, 2, 3].indexOf(5));
     assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  });
+
+  test("escapeRegExp", () => {
+    // 正規表現でエスケープが必要な以下の文字がエスケープされているか確認する
+    // . * + ? ^ $ { } ( ) | [ ] \
+    // https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Regular_Expressions
+    assert.strictEqual(extension.escapeRegExp("."), "\\."); // . -> \.
+    assert.strictEqual(extension.escapeRegExp("*"), "\\*"); // * -> \*
+    assert.strictEqual(extension.escapeRegExp("+"), "\\+"); // + -> \+
+    assert.strictEqual(extension.escapeRegExp("?"), "\\?"); // ? -> \?
+    assert.strictEqual(extension.escapeRegExp("^"), "\\^"); // ^ -> \^
+    assert.strictEqual(extension.escapeRegExp("$"), "\\$"); // $ -> \$
+    assert.strictEqual(extension.escapeRegExp("{"), "\\{"); // { -> \{
+    assert.strictEqual(extension.escapeRegExp("}"), "\\}"); // } -> \}
+    assert.strictEqual(extension.escapeRegExp("("), "\\("); // ( -> \(
+    assert.strictEqual(extension.escapeRegExp(")"), "\\)"); // ) -> \)
+    assert.strictEqual(extension.escapeRegExp("|"), "\\|"); // | -> \|
+    assert.strictEqual(extension.escapeRegExp("["), "\\["); // [ -> \[
+    assert.strictEqual(extension.escapeRegExp("]"), "\\]"); // ] -> \]
+    assert.strictEqual(extension.escapeRegExp("\\"), "\\\\"); // \ -> \\
+
+    // エスケープが必要な文字はエスケープされていて、それ以外はエスケープされていないか確認する
+    assert.strictEqual(extension.escapeRegExp("a."), "a\\.");
+    // 複数文字のエスケープが正しく行われているか確認する
+    assert.strictEqual(extension.escapeRegExp(".*"), "\\.\\*");
+
+    // 以下はエスケープの必要がない文字なので、そのまま出力される
+    assert.strictEqual(extension.escapeRegExp(""), "");
+    assert.strictEqual(extension.escapeRegExp("a"), "a");
+    assert.strictEqual(extension.escapeRegExp("1"), "1");
+    assert.strictEqual(extension.escapeRegExp("あ"), "あ");
+    assert.strictEqual(extension.escapeRegExp("ア"), "ア");
+    assert.strictEqual(extension.escapeRegExp("🍣"), "🍣");
+  });
+
+  test("splitBySpace", () => {
+    // スペースで分割されるか確認する
+    assert.deepStrictEqual(
+      [" ", "a", " ", "b", " ", "c", " "],
+      extension.splitBySpace([" a b c "]),
+    );
+
+    // スペースで分割されるか確認する
+    assert.deepStrictEqual(
+      [" ", "a", "b", "c"],
+      extension.splitBySpace([" a", "b", "c"]),
+    );
+
+    // スペースで分割されるか確認する
+    assert.deepStrictEqual(
+      ["a", " ", "b", "c"],
+      extension.splitBySpace(["a ", "b", "c"]),
+    );
+
+    // スペースで分割されるか確認する
+    assert.deepStrictEqual(
+      ["a", " ", "b", "c"],
+      extension.splitBySpace(["a", " b", "c"]),
+    );
+
+    // スペースで分割されるか確認する
+    assert.deepStrictEqual(
+      ["a", "b", " ", "c"],
+      extension.splitBySpace(["a", "b ", "c"]),
+    );
+
+    // スペースで分割されるか確認する
+    assert.deepStrictEqual(
+      ["a", "b", " ", "c"],
+      extension.splitBySpace(["a", "b", " c"]),
+    );
+
+    // スペースで分割されるか確認する
+    assert.deepStrictEqual(
+      ["a", "b", "c", " "],
+      extension.splitBySpace(["a", "b", "c "]),
+    );
+
+    // スペースで分割されるか確認する
+    assert.deepStrictEqual(
+      ["a", " ", "b", " ", "c"],
+      extension.splitBySpace(["a", " b ", "c"]),
+    );
+
+    // スペースで分割されるか確認する
+    assert.deepStrictEqual(
+      ["a", "   ", "b", "c"],
+      extension.splitBySpace(["a ", "  b", "c"]),
+    );
+  });
+
+  test("getWordSeparatorsRegExp", () => {
+    // 区切り文字の正規表現が正しく生成されるか確認する
+    assert.deepStrictEqual(
+      new RegExp("([\\.]+)", "g"),
+      extension.getWordSeparatorsRegExp("."),
+    );
+
+    // 複数文字の区切り文字の正規表現が正しく生成されるか確認する
+    assert.deepStrictEqual(
+      new RegExp("([\\.\\*]+)", "g"),
+      extension.getWordSeparatorsRegExp(".*"),
+    );
+
+    // 生成した正規表現で文字列が分割されるか確認する
+    assert.deepStrictEqual(
+      ["a", ".", "b", ".", "c"],
+      "a.b.c".split(extension.getWordSeparatorsRegExp(".*")),
+    );
   });
 });
