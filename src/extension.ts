ea961fdfddb66f326cddea8ce6b72a272c2bb4fb
yutotnh <57719497+yutotnh@users.noreply.github.com>
2024-01-13 22:48:56 +0900

    docs: 不要なコメントを削除

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
 let logOutputChannel: vscode.LogOutputChannel;
@@ -7,18 +5,11 @@ let logOutputChannel: vscode.LogOutputChannel;
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "word-divider" is now active!');
-
   const extensionDisplayName = "Word Divider";
   logOutputChannel = vscode.window.createOutputChannel(extensionDisplayName, {
     log: true,
   });
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   context.subscriptions.push(
     vscode.commands.registerCommand(
       "word-divider.cursorWordLeft",
